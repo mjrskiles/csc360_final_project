8d399466572a955b2e538bbc1801919e2016596e
Michael Skiles <mjrskiles@gmail.com>
2018-05-03 15:34:44 -0500

    Added embedded map

--- wrigleyville/src/app/app.module.ts
+++ wrigleyville/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { GameScheduleComponent } from './game-schedule/game-schedule.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { MapComponent } from './map/map.component';
 
 
 @NgModule({
@@ -12,6 +13,7 @@ import { NavbarComponent } from './navbar/navbar.component';
     AppComponent,
     GameScheduleComponent,
     NavbarComponent,
+    MapComponent,
   ],
   imports: [
     BrowserModule
