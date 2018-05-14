791aa800e925d87274ee08748b0583267793baf4
Michael Skiles <mjrskiles@gmail.com>
2018-05-14 18:29:46 -0500

    Router links for each page.

--- wrigleyville/src/app/app-routing.module.ts
+++ wrigleyville/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { TrendsComponent }      from './trends/trends.component';
+import { ContentViewComponent } from './content-view/content-view.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: ContentViewComponent },
+  { path: 'trends', component: TrendsComponent },
+];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
