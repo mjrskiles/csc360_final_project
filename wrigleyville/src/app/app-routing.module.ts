90b9789a952b9f2c8c00e0031afbd1a3d1d160af
Michael Skiles <mjrskiles@gmail.com>
2018-05-15 01:32:30 -0500

    Adjusted navbar style

--- wrigleyville/src/app/app-routing.module.ts
+++ wrigleyville/src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TrendsComponent }      from './trends/trends.component';
 import { ContentViewComponent } from './content-view/content-view.component';
+import { LiveTrafficComponent } from './live-traffic/live-traffic.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: ContentViewComponent },
+  { path: '', redirectTo: '/livetraffic', pathMatch: 'full' },
+  { path: 'livetraffic', component: LiveTrafficComponent },
   { path: 'trends', component: TrendsComponent },
 ];
 
