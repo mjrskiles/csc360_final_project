52c636b4dd1cbaa82f2d8426f2a07b13bcd9d80e
Michael Skiles <mjrskiles@gmail.com>
2018-05-15 15:53:03 -0500

    Added trends mockup

---
+++ wrigleyville/src/app/GameInfo.ts
@@ -0,0 +1,6 @@
+export interface GameInfo {
+  date: string
+  time: string
+  opponent: string
+  isHomeGame: boolean
+}
---
+++ wrigleyville/src/app/TrainETA.ts
@@ -0,0 +1,6 @@
+/* Represents a info for a train ETA that can be rendered by a component. */
+
+export interface TrainETA {
+    destination: string,
+    arrivalTime: string
+}
