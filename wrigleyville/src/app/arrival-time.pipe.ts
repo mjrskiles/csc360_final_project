cb44a06f60e7a38bfacbd003aa7b238817341b2d
Michael Skiles <mjrskiles@gmail.com>
2018-05-20 00:07:04 -0500

    Added better error handling to arrivalTime pipe

--- wrigleyville/src/app/arrival-time.pipe.ts
+++ wrigleyville/src/app/arrival-time.pipe.ts
@@ -6,9 +6,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class ArrivalTimePipe implements PipeTransform {
 
   transform(value: string): string {
-    const arrivalTime = new Date(value).getTime()
-    const minutesUntil = Math.trunc((arrivalTime - Date.now()) / 1000 / 60)
-    return minutesUntil > 1 ? `${minutesUntil} min` : `Due`
+    const arrivalTime = Date.parse(value)
+
+    //Date.parse returns NaN if it fails
+    if (arrivalTime) {
+      const minutesUntil = Math.trunc((arrivalTime - Date.now()) / 1000 / 60)
+      return minutesUntil > 1 ? `${minutesUntil} min` : `Due`
+    }
+    
+    else return "--";
   }
 
 }
