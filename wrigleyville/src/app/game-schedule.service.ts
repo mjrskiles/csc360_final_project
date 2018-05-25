f7c39003b58d918bef23bc7a654ebbb5909010c4
Michael Skiles <mjrskiles@gmail.com>
2018-05-25 17:01:59 -0500

    Successfully fetching cubs schedule from firebase RTDB

--- wrigleyville/src/app/game-schedule.service.ts
+++ wrigleyville/src/app/game-schedule.service.ts
@@ -2,18 +2,20 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { from } from 'rxjs/observable/from';
 import { GameInfo } from './GameInfo';
-import { games } from './games';
+// import { games } from './games';
 import { AngularFireDatabase } from 'angularfire2/database';
 
 @Injectable()
 export class GameScheduleService {
-  private games = games;
+  private games: Observable<any[]>;
 
-  constructor(
-    private db: AngularFireDatabase
-  ) { }
+  constructor(private db: AngularFireDatabase) {
+    // Fetches cubs schedule as a list of games from our Firebase RTDB
+    // https://github.com/angular/angularfire2/blob/master/docs/rtdb/lists.md for more info.
+    this.games = db.list('cubs-games').valueChanges();
+  }
 
-  getSchedule(): Observable<GameInfo[]> {
-    return from([this.games]);
+  getSchedule(): Observable<any[]> {
+    return this.games;
   }
 }
