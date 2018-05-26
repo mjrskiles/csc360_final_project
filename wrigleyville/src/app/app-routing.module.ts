887a5899e588374c79a12b82f50cb4c8f02c0658
Michael Skiles <mjrskiles@gmail.com>
2018-05-26 18:18:34 -0500

    Switched to mobile first layout with bootstrap

--- wrigleyville/src/app/app-routing.module.ts
+++ wrigleyville/src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { TrendsComponent }      from './trends/trends.component';
 import { ContentViewComponent } from './content-view/content-view.component';
 import { LiveTrafficComponent } from './live-traffic/live-traffic.component';
+import { TransitTrackerComponent } from './transit-tracker/transit-tracker.component';
+import { GameScheduleComponent } from './game-schedule/game-schedule.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/livetraffic', pathMatch: 'full' },
   { path: 'livetraffic', component: LiveTrafficComponent },
   { path: 'trends', component: TrendsComponent },
+  { path: 'transit', component: TransitTrackerComponent },
+  { path: 'schedule', component: GameScheduleComponent }
 ];
 
 @NgModule({
