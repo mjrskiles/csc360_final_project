2ef2c9a25059128c442212adcabb81c8009e5961
Michael Skiles <mjrskiles@gmail.com>
2018-05-25 20:57:39 -0500

    Added unit test in game-schedule.service

--- wrigleyville/src/app/game-schedule.service.spec.ts
+++ wrigleyville/src/app/game-schedule.service.spec.ts
@@ -1,15 +1,44 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { GameScheduleService } from './game-schedule.service';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 
 describe('GameScheduleService', () => {
+  let gameScheduleService: GameScheduleService
+  let fireDatabaseServiceSpy: jasmine.SpyObj<AngularFireDatabase>;
+
   beforeEach(() => {
+    // Create a mock service for AngularFireDataBase
+    const fireDBSpy = jasmine.createSpyObj('fireDBSpy', ['list', 'orderByChild', 'startAt']);
+    // need to return an object from list() that has a valueChanges() function, since it gets called in the
+    // service constructor
+    let fakeList = {
+      valueChanges: function() {}
+    }
+    fireDBSpy.list.and.returnValue(fakeList);
+
     TestBed.configureTestingModule({
-      providers: [GameScheduleService]
+      providers: [GameScheduleService, {provide: AngularFireDatabase, useValue: fireDBSpy}]
     });
+    gameScheduleService = TestBed.get(GameScheduleService);
+    fireDatabaseServiceSpy = TestBed.get(AngularFireDatabase);
+
+
   });
 
-  it('should be created', inject([GameScheduleService], (service: GameScheduleService) => {
-    expect(service).toBeTruthy();
-  }));
+  it('should be created', () => {
+    expect(gameScheduleService).toBeTruthy();
+  });
+
+  // Should produce a string in format MM/DD/YY
+  it('should produce a string in format MM/DD/YY', () => {
+    let date = new Date('1990-02-23T12:00:00');
+    let str = gameScheduleService.getFormattedDateString(date);
+    expect(str.length).toBe(8);
+    expect(str[2]).toBe('/')
+    expect(str[5]).toBe('/')
+    expect(parseInt(str.slice(0,2))).toEqual(jasmine.any(Number));
+    expect(parseInt(str.slice(3,5))).toEqual(jasmine.any(Number));
+    expect(parseInt(str.slice(6))).toEqual(jasmine.any(Number));
+  });
 });
--- wrigleyville/src/app/game-schedule.service.ts
+++ wrigleyville/src/app/game-schedule.service.ts
@@ -12,10 +12,24 @@ export class GameScheduleService {
   constructor(private db: AngularFireDatabase) {
     // Fetches cubs schedule as a list of games from our Firebase RTDB
     // https://github.com/angular/angularfire2/blob/master/docs/rtdb/lists.md for more info.
-    this.games = db.list('cubs-games').valueChanges();
+    const today = this.getFormattedDateString(new Date());
+    this.games = db.list('cubs-games', ref => ref.orderByChild("date").startAt(today)).valueChanges();
   }
 
   getSchedule(): Observable<any[]> {
     return this.games;
+  }
+
+  // The dates in our database are formatted as 'MM/DD/YY'
+  getFormattedDateString(date: Date): string {
+    let month = (date.getMonth() + 1).toString(); //Months start at 0
+    month = month.length == 1 ? '0' + month : month; // Add a padding 0 if necessary
+    let day = date.getDate().toString();
+    day = day.length == 1 ? '0' + day : day;
+    let year = date.getFullYear().toString();
+    year = year.slice(2);
+    const todayStr = `${month}/${day}/${year}`;
+    // console.log(`todayStr is ${todayStr}`);
+    return todayStr;
   }
 }
